0e81a0a26ebdde8e6cef28ecf48ec1b6b1f66053
juunini <juuni.ni.i@gmail.com>
2021-05-14 21:10:45 +0900

    Install jest
    - Add tsconfig in absolute path config

--- pages/api/version.ts
+++ pages/api/version.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import packageJson from '../../package.json';
+import packageJson from 'package.json';
 
 export default (_: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ version: packageJson.version });
