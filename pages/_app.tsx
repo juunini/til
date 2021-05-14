1b743177caf8704d622ac0782aa6fb861336f3cb
juunini <juuni.ni.i@gmail.com>
2021-05-14 14:36:54 +0900

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import '../styles/global.css';
+import { AppProps } from 'next/app';
+
+export default function App({ Component, pageProps }: AppProps) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Component {...pageProps} />;
+}
