695919e3de263645c9e19160beee2b2bca883f0b
juunini <juuni.ni.i@gmail.com>
2021-05-21 20:11:12 +0900

    Rename posts to til
    - Change function name list to dir

---
+++ __tests__/lib/til.test.ts
@@ -0,0 +1,31 @@
+import given from 'given2';
+
+import { dir } from 'lib/til';
+
+describe('til', () => {
+  describe('dir', () => {
+    given('dir', () => dir(<number>given.year, <number>given.month));
+
+    context('without any value', () => {
+      it('returns years', () => {
+        expect(given.dir).toContain(2021);
+      });
+    });
+
+    context('with year', () => {
+      given('year', () => 2021);
+
+      it('returns given year\'s months', () => {
+        expect(given.dir).toContain(5);
+      });
+
+      context('with month', () => {
+        given('month', () => 5);
+
+        it('returns given month\'s days', () => {
+          expect(given.dir).toContain(21);
+        });
+      });
+    });
+  });
+});
