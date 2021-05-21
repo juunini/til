294058a35747dfadc2af89309d7c733319fa08f1
juunini <juuni.ni.i@gmail.com>
2021-05-21 20:47:24 +0900

    Create lib/til/contents function
    - Create lib/til/contents test
    - Create TilNotFoundError error

--- __tests__/lib/til.test.ts
+++ __tests__/lib/til.test.ts
@@ -1,6 +1,10 @@
 import given from 'given2';
 
-import { dir } from 'lib/til';
+import {
+  dir,
+  contents,
+} from 'lib/til';
+import { TilNotFoundError } from 'lib/errors';
 
 describe('til', () => {
   describe('dir', () => {
@@ -28,6 +32,32 @@ describe('til', () => {
         it('returns given month\'s days', () => {
           expect(given.dir).toContain('21.md');
         });
+      });
+    });
+  });
+
+  describe('contents', () => {
+    given('year', () => 2021);
+    given('month', () => 5);
+    given('contents', () => contents(
+      given.year.toString(),
+      given.month.toString(),
+      given.day,
+    ));
+
+    context('without file exists', () => {
+      given('day', () => '20.md');
+
+      it('throws tilNotFound error', () => {
+        expect(() => given.contents).toThrowError(TilNotFoundError);
+      });
+    });
+
+    context('with file exists', () => {
+      given('day', () => '21.md');
+
+      it('returns contents', () => {
+        expect(given.contents).not.toBeNull();
       });
     });
   });
--- lib/til.ts
+++ lib/til.ts
@@ -1,16 +1,32 @@
 import fs from 'fs';
 import path from 'path';
+import { TilNotFoundError } from './errors';
 
 const rootPath = __dirname.substring(0, __dirname.length - 4);
 
-export function dir(...paths: string[]): string[] {
-  return fs.readdirSync(
-    path.resolve(
-      rootPath,
-      'til',
-      ...paths.filter((value: string): boolean => value !== undefined && value !== ''),
-    ),
+function makePath(...paths: string[]): string {
+  return path.resolve(
+    rootPath,
+    'til',
+    ...paths.filter((value: string): boolean => value !== undefined && value !== ''),
   );
 }
 
-export const dummy = '';
+export function dir(...paths: string[]): string[] {
+  return fs.readdirSync(
+    makePath(...paths),
+  );
+}
+
+export function contents(...paths: string[]): string {
+  const targetPath = makePath(...paths);
+
+  try {
+    return fs.readFileSync(
+      targetPath,
+      'utf-8',
+    );
+  } catch {
+    throw new TilNotFoundError(targetPath);
+  }
+}
