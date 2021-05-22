69c921951e16b8fb7ba146fe42f97fee0136fa4b
juunini <juuni.ni.i@gmail.com>
2021-05-22 16:22:19 +0900

    Deploy contents

--- pages/api/version.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-import packageJson from 'package.json';
-
-export default (_: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ version: packageJson.version });
-};
