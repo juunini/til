8cb483832f1a32b82a66302adb412597523cbbc4
juunini <juuni.ni.i@gmail.com>
2021-05-22 15:50:52 +0900

    Fix index page

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
-import Home from 'pages/index';
+import Home, { getStaticProps } from 'pages/index';
+import given from 'given2';
 
-describe('index page', () => {
-  it('renders next index page', () => {
-    render(<Home />);
+describe('index', () => {
+  given('year', () => '2021');
 
-    screen.getByText(/Welcome/);
+  describe('Home', () => {
+    it('renders next index page', () => {
+      render(<Home years={[given.year]} />);
+
+      screen.getByText(given.year);
+    });
+  });
+
+  describe('getStaticProps', () => {
+    it('returns contains years object', async () => {
+      const { props: { years } } = await getStaticProps(null);
+
+      expect(years.includes(given.year)).toBeTruthy();
+    });
   });
 });
