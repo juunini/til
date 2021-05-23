c56da36343d4f556ce4d1820dfa3346eb76aac5d
juunini <juuni.ni.i@gmail.com>
2021-05-23 10:52:52 +0900

    Create Header layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,13 @@ import { AppProps } from 'next/app';
 
 import Head from 'layout/Head';
 import '../styles/globals.css';
+import Header from 'layout/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head />
+      <Header />
       <Component {...pageProps} />
     </>
   );
