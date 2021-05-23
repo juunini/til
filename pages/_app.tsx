fe3e4faf31f05331de5217ac913e51dce18e264c
juunini <juuni.ni.i@gmail.com>
2021-05-23 10:07:12 +0900

    Add Head layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,13 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { AppProps } from 'next/app';
-import Head from 'next/head';
 
+import Head from 'layout/Head';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>juunini&apos;s til</title>
-        <meta name="description" content="juunini's til" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+      <Head />
       <Component {...pageProps} />
     </>
   );
