c20251fe06fc9c5e9d42f5d09e9b2aa94bbd4758
juunini <juuni.ni.i@gmail.com>
2021-09-21 22:06:26 +0900

    Add climbing order in ls function

--- __tests__/lib/til/ls.test.ts
+++ __tests__/lib/til/ls.test.ts
@@ -5,7 +5,7 @@ import ls from 'lib/til/ls';
 describe('ls', () => {
   context('without any value', () => {
     it('should returns til directory\'s year folder list', () => {
-      expect(ls()).toContain('2021'); // 2021년
+      expect(ls()).toContain('2021'); // 2021년 폴더
     });
   });
 
@@ -13,8 +13,8 @@ describe('ls', () => {
     given('year', () => '2021');
 
     it('should returns til/2021 in month folder list', () => {
-      expect(ls(given.year)).toContain('8'); // 8월
-      expect(ls(given.year)).toContain('9'); // 9월
+      expect(ls(given.year)).toContain('8'); // 8월 폴더
+      expect(ls(given.year)).toContain('9'); // 9월 폴더
     });
   });
 });
