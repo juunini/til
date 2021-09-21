783bb31cef6a68fe2b0782d3af9ba05496c6437d
juunini <juuni.ni.i@gmail.com>
2021-09-22 00:29:02 +0900

    Remove description

--- layout/Head.tsx
+++ layout/Head.tsx
@@ -4,7 +4,6 @@ export default function Head(): JSX.Element {
   return (
     <NextHead>
       <title>{'juunini\'s til'}</title>
-      <meta name="description" content="juunini's til" />
       <link rel="icon" href="/favicon.ico" />
     </NextHead>
   );
